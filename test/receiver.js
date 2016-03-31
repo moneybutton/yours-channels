7ac5214ba65b845cd25847e4bb52adae6f079da0
Clemens Ley <ley.clemens@gmail.com>
2016-03-30 18:30:44 -0700

    formatting, added test cases, and sender.asyncAcceptPayment

--- test/receiver.js
+++ test/receiver.js
@@ -1,41 +1,27 @@
+/* global describe,it */
 'use strict'
 
 let should = require('should')
 let Receiver = require('../lib/receiver.js')
-let Sender = require('../lib/sender.js')
 let Privkey = require('fullnode/lib/privkey')
 let Pubkey = require('fullnode/lib/pubkey')
-let Keypair = require('fullnode/lib/keypair')
-let Address = require('fullnode/lib/address')
-let Script = require('fullnode/lib/script')
 let Tx = require('fullnode/lib/tx')
-let Txout = require('fullnode/lib/txout')
-let Txbuilder = require('fullnode/lib/txbuilder')
-let Txverifier = require('fullnode/lib/txverifier')
+let Txoutmap = require('fullnode/lib/txoutmap')
 let BN = require('fullnode/lib/bn')
 let asink = require('asink')
 
 describe('Receiver', function () {
-  // the private address of sender
-  let senderPrivkey = Privkey().fromBN(BN(1))
-  let senderPubkey = Pubkey().fromPrivkey(senderPrivkey)
-  let senderAddress = Address().fromPubkey(senderPubkey)
-
-  // the private key used to create the multig addr
-  let senderMsPrivkey = Privkey().fromBN(BN(2))
-
   // the receiver's pub key used to create the multisig
   let receiverMsPrivkey = Privkey().fromBN(BN(3))
-  let receiverMsPubkey = Pubkey().fromPrivkey(receiverMsPrivkey)
 
   // the receivers private address
   let receiverPrivKey = Privkey().fromBN(BN(4))
   let receiverPubkey = Pubkey().fromPrivkey(receiverPrivKey)
-  let receiverAddress = Address().fromPubkey(receiverPubkey)
 
   let consts = {
-    partialPaymentTx: '0100000001ad2208aeb1993e405f41c54287092b8317204a7e9f8a38182e76a4c7223b952a000000009200473044022030a4fa6518d94d158686bffcd8206f8350ad51e82419977512187fed0f0b5d4302204b15818763992bdf5b7d44c52cc32e0530b66e59961b59cddbcbaa988f62c860010047522102c6047f9441ed7d6d3045406e95c07cd85c778e4b8cef3ca7abac09b95c709ee52102f9308a019258c31049344f85f89d5229b531c845836f99b08601f113bce036f952aeffffffff03d0dd0600000000001976a914751e76e8199196d454941c45d1b3a323f1433bd688ac50c30000000000001976a914c42e7ef92fdb603af844d064faad95db9bcdfd3d88ac50ce9000000000001976a914751e76e8199196d454941c45d1b3a323f1433bd688ac00000000',
-    completelySignedTx: '0100000001ad2208aeb1993e405f41c54287092b8317204a7e9f8a38182e76a4c7223b952a00000000da00473044022030a4fa6518d94d158686bffcd8206f8350ad51e82419977512187fed0f0b5d4302204b15818763992bdf5b7d44c52cc32e0530b66e59961b59cddbcbaa988f62c86001483045022100d4bca42fde4982180497bc0079e10b4762f255ae57eadebbcbb28b0a92ac7e9102207e91f584ac06ca074cd96eeb57f541f5393b27c5888267e30358292b108a5fc40147522102c6047f9441ed7d6d3045406e95c07cd85c778e4b8cef3ca7abac09b95c709ee52102f9308a019258c31049344f85f89d5229b531c845836f99b08601f113bce036f952aeffffffff03d0dd0600000000001976a914751e76e8199196d454941c45d1b3a323f1433bd688ac50c30000000000001976a914c42e7ef92fdb603af844d064faad95db9bcdfd3d88ac50ce9000000000001976a914751e76e8199196d454941c45d1b3a323f1433bd688ac00000000'
+    fundingTx: '01000000010000000000000000000000000000000000000000000000000000000000000000000000006a47304402200d4b8ab538542f844a3e5f417a7ee9d1811e745cd093b0a8513a0aec043409fd022077e58ecf722e335232c29231ca156b2efae843ba9a3b809cd6aff5f2bd1726a8012103774ae7f858a9411e5ef4246b70c65aac5649980be5c17891bbec17895da008cbffffffff02809698000000000017a9149af9b52da033d663b8eef59d586c7a4e01cf02168770235d05000000001976a914185140bb54704a9e735016faa7a8dbee4449bddc88ac00000000',
+    partialPaymentTx: '0100000001b806dccfc74a52092ffd075daf46449380066aef95202d7f1b9ba00a22dc032e00000000920047304402203d5e0bb5de1f08f72a4c1c787a779ae835106cae95955681a2ff560d9b08ce9b022031f7a93014f5cb252225cfef87e2cc7c8bdb23205368051c1559243d261d060b010047522102c6047f9441ed7d6d3045406e95c07cd85c778e4b8cef3ca7abac09b95c709ee52102f9308a019258c31049344f85f89d5229b531c845836f99b08601f113bce036f952aeffffffff03d0dd0600000000001976a914751e76e8199196d454941c45d1b3a323f1433bd688ac50c30000000000001976a914c42e7ef92fdb603af844d064faad95db9bcdfd3d88ac50ce9000000000001976a914751e76e8199196d454941c45d1b3a323f1433bd688ac00000000',
+    completelySignedTx: '0100000001b806dccfc74a52092ffd075daf46449380066aef95202d7f1b9ba00a22dc032e00000000da0047304402203d5e0bb5de1f08f72a4c1c787a779ae835106cae95955681a2ff560d9b08ce9b022031f7a93014f5cb252225cfef87e2cc7c8bdb23205368051c1559243d261d060b01483045022100e7b8f02307f30001f21049eac5a5a4d7a25c789f1d8383fb84fc8bae2095e3f602202e08017f2d906efc800b8cc883cff26a5c4824f645a0450b072bfe09d662188f0147522102c6047f9441ed7d6d3045406e95c07cd85c778e4b8cef3ca7abac09b95c709ee52102f9308a019258c31049344f85f89d5229b531c845836f99b08601f113bce036f952aeffffffff03d0dd0600000000001976a914751e76e8199196d454941c45d1b3a323f1433bd688ac50c30000000000001976a914c42e7ef92fdb603af844d064faad95db9bcdfd3d88ac50ce9000000000001976a914751e76e8199196d454941c45d1b3a323f1433bd688ac00000000'
   }
 
   it('should exist', function () {
@@ -44,10 +30,9 @@ describe('Receiver', function () {
     should.exist(Receiver())
   })
 
-  describe('signPaymentTx', function () {
+  describe('asyncSignPaymentTx', function () {
     it('asyncSignPaymentTx signs a tx', function () {
       return asink(function *() {
-        let sender = Sender(senderAddress, senderMsPrivkey, receiverMsPubkey, receiverAddress)
         let receiver = Receiver(receiverMsPrivkey, receiverPubkey)
         let partialPaymentTx = Tx().fromString(consts.partialPaymentTx)
         let completelySignedTx = yield receiver.asyncSignPaymentTx(partialPaymentTx)
@@ -57,26 +42,31 @@ describe('Receiver', function () {
     })
   })
 
-
   describe('verifyTx', function () {
-/*
     it('verifyTx should verify a tx', function () {
-      return asink(function *() {
-        let sender = Sender(senderAddress, senderMsPrivkey, receiverMsPubkey, receiverAddress)
-        let receiver = Receiver(receiverMsPrivkey, receiverPubkey)
-        let fundingTx = Tx().fromString(consts.fundingTx)
-        let paymentTx = Tx().fromString(consts.completelySignedTx)
-        receiver.txoutmap = fundingTx.txouts
-        let verification = receiver.asyncVerifyTx(paymentTx)
-      }, this)
+      let receiver = Receiver(receiverMsPrivkey, receiverPubkey)
+      let fundingTx = Tx().fromString(consts.fundingTx)
+      let paymentTx = Tx().fromString(consts.completelySignedTx)
+      let txoutmap = Txoutmap()
+      txoutmap.addTx(fundingTx)
+      receiver.txoutmap = txoutmap
+      let verification = receiver.verifyTx(paymentTx)
+      verification.should.equal(true)
     })
-*/
   })
 
-  describe('acceptPayment', function () {
-    it('acceptPayment should exist', function () {
-      let receiver = Receiver()
-      should.exist(receiver.acceptPayment)
+  describe('asyncAcceptPayment', function () {
+    it('asyncAcceptPayment should accept a payment if he can sign it and it is verifyable', function () {
+      return asink(function *() {
+        let receiver = Receiver(receiverMsPrivkey, receiverPubkey)
+        let fundingTx = Tx().fromString(consts.fundingTx)
+        let partialPaymentTx = Tx().fromString(consts.partialPaymentTx) // this is the difference to asyncVerifyTx
+        let txoutmap = Txoutmap()
+        txoutmap.addTx(fundingTx)
+        receiver.txoutmap = txoutmap
+        let acceptance = yield receiver.asyncAcceptPayment(partialPaymentTx)
+        acceptance.should.equal(true)
+      }, this)
     })
   })
 
@@ -86,5 +76,4 @@ describe('Receiver', function () {
       should.exist(receiver.closeChannel)
     })
   })
-
 })
