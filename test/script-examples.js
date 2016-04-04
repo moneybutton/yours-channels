9b6e5973a2ef93d6baf614873d778f046cff6fa8
Ryan X. Charles <ryanxcharles@gmail.com>
2016-04-03 18:28:07 -0700

    example of putting CLTV in a p2sh tx

--- test/script-examples.js
+++ test/script-examples.js
@@ -22,6 +22,10 @@ describe('Script Examples', function () {
       // also for the transaction locktime to be at least 100. The sequence
       // number seqnum is set to an arbitrary value less than 0xffffffff, which
       // is necessary to enable CLTV.
+      //
+      // scriptPubkey: OP_CHECKLOCKTIMEVERIFY OP_DROP <pubkey> OP_CHECKSIG
+      // scriptSig: <sig> <nlocktime>
+
       let scriptnlocktime = 100
       let privkey = Privkey().fromRandom()
       let pubkey = Pubkey().fromPrivkey(privkey)
@@ -64,14 +68,81 @@ describe('Script Examples', function () {
       txb.tx.txins[0].setScript(scriptSig)
       Txverifier.verify(txb.tx, txb.utxoutmap, Interp.SCRIPT_VERIFY_P2SH | Interp.SCRIPT_VERIFY_CHECKLOCKTIMEVERIFY).should.equal(true)
     })
+
+    it('should lock up funds until block 100 with a p2sh transaction', function () {
+      // This example is almost the same as the previous example - it spends to
+      // an output that requires a normal signature and also for the
+      // transaction locktime to be at least 100. The sequence number seqnum is
+      // set to an arbitrary value less than 0xffffffff, which is necessary to
+      // enable CLTV. The difference is that it is a p2sh transaction, so much
+      // of the logic is in the redeemScript. Also note that the subscript
+      // changes when doing tx sign - in the p2sh case, the subscript is the
+      // redeemScript, not the scriptPubkey.
+      //
+      // scriptPubkey: OP_HASH160 <p2shaddress> OP_EQUAL
+      // scriptSig: <sig> <nlocktime> <redeemScript>
+      // redeemScript: OP_CHECKLOCKTIMEVERIFY OP_DROP <pubkey> OP_CHECKSIG
+
+      let scriptnlocktime = 100
+      let privkey = Privkey().fromRandom()
+      let pubkey = Pubkey().fromPrivkey(privkey)
+      let keypair = Keypair(privkey, pubkey)
+      let redeemScript = Script()
+        .writeOpcode(Opcode.OP_CHECKLOCKTIMEVERIFY)
+        .writeOpcode(Opcode.OP_DROP)
+        .writeBuffer(pubkey.toBuffer())
+        .writeOpcode(Opcode.OP_CHECKSIG)
+      let scriptSig = Script()
+        .writeOpcode(Opcode.OP_0) // signature - will be replaced with actual signature
+        .writeBN(BN(scriptnlocktime))
+        .writeBuffer(redeemScript.toBuffer())
+      let scriptPubkey = Script()
+        .writeOpcode(Opcode.OP_HASH160)
+        .writeBuffer(Hash.sha256ripemd160(redeemScript.toBuffer()))
+        .writeOpcode(Opcode.OP_EQUAL)
+
+      let txb = Txbuilder()
+      let txhashbuf = new Buffer(32)
+      txhashbuf.fill(0)
+      let txoutnum = 0
+      let txout = Txout(BN(500000)).setScript(scriptPubkey)
+      let seqnum = 0xf0f0f0f0
+      txb.fromScript(txhashbuf, txoutnum, txout, scriptSig, seqnum)
+      txb.setChangeAddress(Address().fromPrivkey(Privkey().fromRandom()))
+      txb.toAddress(BN(100000), Address().fromPrivkey(Privkey().fromRandom()))
+
+      let sig, txnlocktime
+
+      // tx lock time too low - tx invalid
+      txnlocktime = 99
+      txb.setNLocktime(txnlocktime)
+      txb.build()
+      sig = txb.getSig(keypair, Sig.SIGHASH_ALL, 0, redeemScript)
+      scriptSig.chunks[0] = Script().writeBuffer(sig.toTxFormat()).chunks[0]
+      txb.tx.txins[0].setScript(scriptSig)
+      Txverifier.verify(txb.tx, txb.utxoutmap, Interp.SCRIPT_VERIFY_P2SH | Interp.SCRIPT_VERIFY_CHECKLOCKTIMEVERIFY).should.equal(false)
+
+      // tx lock time high enough - tx valid
+      txnlocktime = 100
+      txb.setNLocktime(txnlocktime)
+      txb.build()
+      sig = txb.getSig(keypair, Sig.SIGHASH_ALL, 0, redeemScript)
+      scriptSig.chunks[0] = Script().writeBuffer(sig.toTxFormat()).chunks[0]
+      txb.tx.txins[0].setScript(scriptSig)
+      Txverifier.verify(txb.tx, txb.utxoutmap, Interp.SCRIPT_VERIFY_P2SH | Interp.SCRIPT_VERIFY_CHECKLOCKTIMEVERIFY).should.equal(true)
+    })
   })
 
   describe('Hash Time Lock (HTL)', function () {
     it('should enable spending funds when sig and value that hashes correctly is in input', function () {
       // This example spends to an output that requires both a signature and some
-      // secret value tohashbuf which correctly hashes to hashbuf
-      let tohashbuf = new Buffer('this is a secret string')
-      let hashbuf = Hash.sha256(tohashbuf)
+      // secret value secretbuf which correctly hashes to hashbuf
+      //
+      // scriptPubkey: OP_SHA256 <hash> OP_EQUALVERIFY <pubkey> OP_CHECKSIG
+      // scriptSig: <sig> <secret>
+
+      let secretbuf = new Buffer('this is a secret string')
+      let hashbuf = Hash.sha256(secretbuf)
 
       let privkey = Privkey().fromRandom()
       let pubkey = Pubkey().fromPrivkey(privkey)
@@ -85,7 +156,7 @@ describe('Script Examples', function () {
         .writeOpcode(Opcode.OP_CHECKSIG)
       let scriptSig = Script()
         .writeOpcode(Opcode.OP_0) // signature - will be replaced with actual signature
-        .writeBuffer(tohashbuf) // secret value
+        .writeBuffer(secretbuf) // secret value
 
       let txb = Txbuilder()
       let txhashbuf = new Buffer(32)
