fc6688682ac7d8312a8e4ae7964f8571b49870fb
Clemens Ley <ley.clemens@gmail.com>
2016-04-26 16:37:53 +0200

    aaded test cases for secret

--- test/secret.js
+++ test/secret.js
@@ -3,7 +3,7 @@
 let should = require('should')
 let asink = require('asink')
 let Secret = require('../lib/secret.js')
-let Hash = require('fullnode/lib/hash')
+// let Hash = require('fullnode/lib/hash')
 
 describe('Secret', function () {
   it('should exist', function () {
@@ -12,17 +12,59 @@ describe('Secret', function () {
     should.exist(Secret())
   })
 
+  describe('#generateBuf', function () {
+    it('generateBuf should generate a buffer', function () {
+      return asink(function *() {
+        let secret = Secret()
+        secret.generateBuf()
+        should.exist(secret.buf)
+      }, this)
+    })
+  })
+
+  describe('#asyncGenerateHash', function () {
+    it('asyncGenerateHash should generate a hash', function () {
+      return asink(function *() {
+        let secret = Secret()
+        secret.generateBuf()
+        should.exist(secret.buf)
+        yield secret.asyncGenerateHash()
+        should.exist(secret.hash)
+      }, this)
+    })
+  })
+
   describe('#asyncCheck', function () {
     it('asyncCheck should check if buf hashes to hash', function () {
       return asink(function *() {
         let secret = Secret()
-        let buf = new Buffer('abc')
-        let hashedBuf = yield Hash.asyncSha256ripemd160(buf)
-
-        secret.buf = buf
-        secret.hash = hashedBuf
+        secret.generateBuf()
+        should.exist(secret.buf)
+        yield secret.asyncGenerateHash()
+        should.exist(secret.hash)
         let result = yield secret.asyncCheck()
         result.should.equal(true)
+      }, this)
+    })
+  })
+
+  describe('#hidden', function () {
+    it('hidden should remove the buf from a secret', function () {
+      return asink(function *() {
+        let secret = Secret()
+        secret.generateBuf()
+        should.exist(secret.buf)
+        yield secret.asyncGenerateHash()
+        should.exist(secret.hash)
+        let hiddenSecret = secret.hidden()
+
+        // secret should not change
+        should.exist(secret.buf)
+        should.exist(secret.hash)
+
+        // hiddenSecret should not have a buf
+        should.not.exist(hiddenSecret.buf)
+        should.exist(hiddenSecret.hash)
       }, this)
     })
   })
