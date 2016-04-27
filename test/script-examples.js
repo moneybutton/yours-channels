1f45ef1815e3d4da8b057e2072e5de6ca17da7d0
Ryan X. Charles <ryanxcharles@gmail.com>
2016-04-27 14:37:06 -0700

    script examples: alice builds RHTLC output

--- test/script-examples.js
+++ test/script-examples.js
@@ -351,14 +351,22 @@ describe('Script Examples', function () {
       let alice = {}
       let bob = {}
 
+      // Creating addresses to receive payments.
+      alice.paymentKeypair = Keypair().fromRandom()
+      alice.paymentAddress = Address().fromPubkey(alice.paymentKeypair.pubkey)
+      bob.paymentKeypair = Keypair().fromRandom()
+      bob.paymentAddress = Address().fromPubkey(alice.paymentKeypair.pubkey)
+      alice.otherPaymentPubkey = bob.paymentKeypair.pubkey
+      bob.otherPaymentPubkey = alice.paymentKeypair.pubkey
+
       // Creating the multisig address.
       alice.msKeypair = Keypair().fromRandom()
       bob.msKeypair = Keypair().fromRandom()
-      alice.otherPubkey = bob.msKeypair.pubkey
-      bob.otherPubkey = alice.msKeypair.pubkey
-      alice.msRedeemScript = Script().fromPubkeys(2, [alice.msKeypair.pubkey, alice.otherPubkey])
+      alice.otherMsPubkey = bob.msKeypair.pubkey
+      bob.otherMsPubkey = alice.msKeypair.pubkey
+      alice.msRedeemScript = Script().fromPubkeys(2, [alice.msKeypair.pubkey, alice.otherMsPubkey])
       alice.msAddress = Address().fromRedeemScript(alice.msRedeemScript)
-      bob.msRedeemScript = Script().fromPubkeys(2, [bob.msKeypair.pubkey, bob.otherPubkey])
+      bob.msRedeemScript = Script().fromPubkeys(2, [bob.msKeypair.pubkey, bob.otherMsPubkey])
       bob.msAddress = Address().fromRedeemScript(bob.msRedeemScript)
 
       // Confirm that Alice and Bob have created the same address.
@@ -391,6 +399,9 @@ describe('Script Examples', function () {
       // payments, to Alice can't send the full amount back to herself after
       // sending payments to Bob. Alice builds this transaction, but doesn't
       // sign it, and requests that Bob signs it and sends it back.
+
+      // Alice begins creating the commitment tx by first specifying that it
+      // comes from the funding tx.
       alice.fundingTxHashbuf = alice.fundingTxb.tx.hash()
       alice.fundingTxOutnum = 0
       alice.fundingTxAmount = BN(100000)
@@ -398,11 +409,65 @@ describe('Script Examples', function () {
       alice.revokeHash1 = Hash.sha256ripemd160(alice.revokeSecret1)
       alice.commitmentTxb1 = Txbuilder()
       alice.commitmentTxb1.fromScripthashMultisig(alice.fundingTxHashbuf, alice.fundingTxOutnum, alice.fundingTxb.tx.txouts[0], alice.msRedeemScript)
-      alice.commitmentTxb1.setChangeScript(alice.changeScript)
 
-      bob.fundingTxHashbuf = alice.fundingTxHashbuf
-      bob.fundingTxOutnum = alice.fundingTxOutnum
-      bob.fundingTxAmount = alice.fundingTxAmount
+      // Alice needs the hash of Bob's secrets to continue building the tx.
+      // Alice and bob generate revoke secrets and HTLC secrets and share their
+      // hashes.
+      alice.revokeSecret1 = Random.getRandomBuffer(32)
+      alice.revokeHash1 = Hash.sha256ripemd160(alice.revokeSecret1)
+      bob.revokeSecret1 = Random.getRandomBuffer(32)
+      bob.revokeHash1 = Hash.sha256ripemd160(alice.revokeSecret1)
+      alice.htlcSecret1 = Random.getRandomBuffer(32)
+      alice.htlcHash1 = Hash.sha256ripemd160(alice.htlcSecret1)
+      bob.htlcSecret1 = Random.getRandomBuffer(32)
+      bob.htlcHash1 = Hash.sha256ripemd160(alice.htlcSecret1)
+      alice.otherRevokeHash1 = bob.revokeHash1
+      alice.otherHtlcHash1 = bob.htlcHash1
+      bob.otherRevokeHash1 = alice.revokeHash1
+      bob.otherHtlcHash1 = alice.htlcHash1
+
+      // Alice creates the RHTLC output to herself.
+      alice.refundOutputScript = Script()
+        .writeOpcode(Opcode.OP_IF)
+          .writeBuffer(alice.otherPaymentPubkey.toBuffer())
+          .writeOpcode(Opcode.OP_CHECKSIGVERIFY)
+          .writeOpcode(Opcode.OP_HASH160)
+          .writeBuffer(alice.revokeHash1)
+          .writeOpcode(Opcode.OP_EQUALVERIFY)
+        .writeOpcode(Opcode.OP_ELSE)
+          .writeOpcode(Opcode.OP_IF)
+            .writeBN(BN(6 * 24)) // one day = six blocks per hour for 24 hours
+            .writeOpcode(Opcode.OP_CHECKSEQUENCEVERIFY)
+            .writeOpcode(Opcode.OP_DROP)
+            .writeBuffer(alice.otherPaymentPubkey.toBuffer())
+            .writeOpcode(Opcode.OP_CHECKSIGVERIFY)
+            .writeOpcode(Opcode.OP_HASH160)
+            .writeBuffer(alice.otherHtlcHash1)
+            .writeOpcode(Opcode.OP_EQUALVERIFY)
+          .writeOpcode(Opcode.OP_ELSE)
+            .writeBN(BN(6 * 48)) // two days = six blocks per hour for 48 hours
+            .writeOpcode(Opcode.OP_CHECKSEQUENCEVERIFY)
+            .writeOpcode(Opcode.OP_DROP)
+            .writeBuffer(alice.paymentKeypair.pubkey.toBuffer())
+            .writeOpcode(Opcode.OP_CHECKSIGVERIFY)
+          .writeOpcode(Opcode.OP_ENDIF)
+        .writeOpcode(Opcode.OP_ENDIF)
+
+      // Alice does NOT create an HTLC output to Bob yet - since that would
+      // only send 0 bitcoins at this point.
+
+      // Alice sets the RHTLC output to herself as the change of the txbuilder
+      // - that is, the total amount sent back to herself will be everything
+      // not sent in one of the other outputs minus the fee. Since there will
+      // be no other outputs, it's just the total input amount minus the fee.
+      alice.commitmentTxb1.setChangeScript(alice.refundOutputScript)
+
+      // Alice now builds the tx. It has only one output - the RHTLC output
+      // going back to Alice.
+      alice.commitmentTxb1.build()
+
+      // Alice does not yet sign the commitment tx. She sends it to Bob.
+      bob.commitmentTx = Txbuilder().fromJSON(alice.commitmentTxb1.toJSON())
 
       // TODO: Not finished!!!
     })
