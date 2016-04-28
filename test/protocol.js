15b1a0687e513e67687f4b335f7f085f00faeee2
Clemens Ley <ley.clemens@gmail.com>
2016-04-28 14:59:52 +0200

    cleaned up interface of protocol

--- test/protocol.js
+++ test/protocol.js
@@ -4,20 +4,16 @@ let should = require('should')
 let asink = require('asink')
 let Privkey = require('fullnode/lib/privkey')
 let Pubkey = require('fullnode/lib/pubkey')
-let Script = require('fullnode/lib/script')
-let Txout = require('fullnode/lib/txout')
-let Address = require('fullnode/lib/address')
-let BN = require('fullnode/lib/bn')
 
 let Agent = require('../lib/agent.js')
 let Protocol = require('../lib/protocol.js')
 
 describe('Protocol', function () {
   let privkey = Privkey().fromRandom()
-  let pubkey = Pubkey().fromPrivkey(privkey)
-  let address = Address().fromPubkey(pubkey)
+  // let pubkey = Pubkey().fromPrivkey(privkey)
+  // let address = Address().fromPubkey(pubkey)
   let msPrivkey = Privkey().fromRandom()
-//  let msPubkey = Pubkey().fromPrivkey(msPrivkey)
+  // let msPubkey = Pubkey().fromPrivkey(msPrivkey)
 
   let otherPrivkey = Privkey().fromRandom()
   let otherPubkey = Pubkey().fromPrivkey(otherPrivkey)
@@ -30,54 +26,15 @@ describe('Protocol', function () {
     should.exist(Protocol())
   })
 
-  describe('#asyncInitialize', function () {
-    it('asyncInitialize should initialize agent and genereate secrets', function () {
+  describe('#asyncOpenChannel', function () {
+    it('asyncOpenChannel should store the other agents addeses and build a multisig address', function () {
       return asink(function *() {
         let agent = Agent()
+        yield agent.asyncInitialize(privkey, msPrivkey)
         let protocol = Protocol(agent)
-        yield protocol.asyncInitialize(privkey, msPrivkey)
-
-        protocol.agent.initialized.should.equal(true)
-        should.exist(protocol.agent.revocationSecret)
-        should.exist(protocol.agent.htlcSecret)
-      }, this)
-    })
-  })
-
-  describe('#openChannel', function () {
-    it('openChannel should store the other agents addeses and build a multisig address', function () {
-      return asink(function *() {
-        let agent = Agent()
-        let protocol = Protocol(agent)
-        yield protocol.asyncInitialize(privkey, msPrivkey)
-        yield protocol.openChannel(otherPubkey, otherMsPubkey)
-
+        yield protocol.asyncOpenChannel(otherPubkey, otherMsPubkey)
+        should.exist(protocol.agent.other)
         should.exist(protocol.agent.multisig)
-        should.exist(protocol.agent.revocationSecret)
-        should.exist(protocol.agent.htlcSecret)
-      }, this)
-    })
-  })
-
-  describe('#createFundingTx', function () {
-    it('createFundingTx should create a funding transaction', function () {
-      return asink(function *() {
-        let agent = Agent()
-        let protocol = Protocol(agent)
-        yield protocol.asyncInitialize(privkey, msPrivkey)
-        yield protocol.openChannel(otherPubkey, otherMsPubkey)
-
-        let scriptout = Script().fromString('OP_DUP OP_HASH160 20 0x' + address.hashbuf.toString('hex') + ' OP_EQUALVERIFY OP_CHECKSIG')
-        let amount = BN(2e7)
-        let txhashbuf = new Buffer(32).fill(0)
-        let txoutnum = 0
-        let txoutamount = BN(1e8)
-        let txout = Txout(txoutamount, scriptout)
-        yield protocol.createFundingTx(amount, txhashbuf, txoutnum, txout, pubkey)
-
-        should.exist(protocol.agent.fundingTx)
-        should.exist(protocol.agent.fundingTxhashbuf)
-        should.exist(protocol.agent.fundingTxout)
       }, this)
     })
   })
