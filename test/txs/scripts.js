f56b1debab7cca0cbc1daec6d3ede49fe6398654
Clemens Ley <ley.clemens@gmail.com>
2016-05-30 18:04:07 +0200

    moved scripts.js to txs folder, deleted cnl-txs-builder

--- test/txs/scripts.js
+++ test/txs/scripts.js
@@ -2,7 +2,7 @@
 'use strict'
 let should = require('should')
 // let asink = require('asink')
-let Scripts = require('../lib/scripts.js')
+let Scripts = require('../../lib/txs/scripts.js')
 // let Hash = require('fullnode/lib/hash')
 
 describe('Scripts', function () {
