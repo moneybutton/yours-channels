631c2f1a375999ee11399a36ebab14ca3e98e11d
Clemens Ley <ley.clemens@gmail.com>
2016-06-08 17:51:19 +0200

    added test cases for wallet

---
+++ test/wallet.js
@@ -0,0 +1,33 @@
+/* global describe,it */
+'use strict'
+let should = require('should')
+let asink = require('asink')
+let Privkey = require('fullnode/lib/privkey')
+let Pubkey = require('fullnode/lib/pubkey.js')
+let Address = require('fullnode/lib/address.js')
+let Wallet = require('../lib/wallet.js')
+let Bn = require('fullnode/lib/bn')
+
+describe('Wallet', function () {
+  let privkey = Privkey().fromRandom()
+  let pubkey = Pubkey().fromPrivkey(privkey)
+  let address = Address().fromPubkey(pubkey)
+
+  it('should exist', function () {
+    should.exist(Wallet)
+    should.exist(new Wallet())
+  })
+
+  describe('#getUnspentOutput', function () {
+    it('should return an unspent output', function () {
+      return asink(function *() {
+        let wallet = new Wallet()
+        let output = wallet.getUnspentOutput(Bn(1e8), address)
+
+        should.exist(output.txhashbuf)
+        should.exist(output.txoutnum)
+        should.exist(output.txout)
+      }, this)
+    })
+  })
+})
