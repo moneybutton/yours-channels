ce2c2f55917dfb0670f3d84e72bd6141718a5660
Ryan X. Charles <ryan@yours.network>
2016-06-12 17:15:21 -0700

    MsgOpenChannel -> MsgOpen

--- test/msgs/msg-open.js
+++ test/msgs/msg-open.js
@@ -2,27 +2,27 @@
 'use strict'
 let Bn = require('yours-bitcoin/lib/bn')
 let KeyPair = require('yours-bitcoin/lib/key-pair')
-let MsgOpenChannel = require('../../lib/msgs/msg-open-channel')
+let MsgOpen = require('../../lib/msgs/msg-open')
 let asink = require('asink')
 let should = require('should')
 
-describe('MsgOpenChannel', function () {
+describe('MsgOpen', function () {
   it('should exist', function () {
-    should.exist(MsgOpenChannel)
-    should.exist(new MsgOpenChannel())
+    should.exist(MsgOpen)
+    should.exist(new MsgOpen())
   })
 
   describe('#constructor', function () {
     it('should set the default command name', function () {
-      let msg = new MsgOpenChannel()
-      msg.cmd.should.equal('open-channel')
+      let msg = new MsgOpen()
+      msg.cmd.should.equal('open')
     })
   })
 
   describe('#setPubKey', function () {
     it('should set the pubKey', function () {
       let pubKey = KeyPair.fromRandom().pubKey
-      let msg = new MsgOpenChannel()
+      let msg = new MsgOpen()
       msg.setPubKey(pubKey)
       should.exist(msg.args.pubKey)
     })
@@ -32,7 +32,7 @@ describe('MsgOpenChannel', function () {
     it('should get the pubKey from this msg', function () {
       return asink(function * () {
         let pubKey = KeyPair.fromRandom().pubKey
-        let msg = new MsgOpenChannel()
+        let msg = new MsgOpen()
         msg.setPubKey(pubKey)
         let pubKey2 = yield msg.asyncGetPubKey()
         pubKey2.toString().should.equal(pubKey.toString())
@@ -43,7 +43,7 @@ describe('MsgOpenChannel', function () {
   describe('#setAmount', function () {
     it('should set the amount', function () {
       let amount = Bn(1e8)
-      let msg = new MsgOpenChannel()
+      let msg = new MsgOpen()
       msg.setAmount(amount)
       should.exist(msg.args.amount)
     })
@@ -53,7 +53,7 @@ describe('MsgOpenChannel', function () {
     it('should get the amount from this msg', function () {
       return asink(function * () {
         let amount = Bn(1e8)
-        let msg = new MsgOpenChannel()
+        let msg = new MsgOpen()
         msg.setAmount(amount)
         let amount2 = msg.getAmount()
         amount2.eq(amount).should.equal(true)
