c6c6a8b74966f23b5fd6d1048d3559004c42e1c0
Ryan X. Charles <ryan@yours.network>
2016-06-24 15:35:29 -0700

    the channel id is the multiSigAddr string

--- test/channel.js
+++ test/channel.js
@@ -1,11 +1,13 @@
 /* global describe,it */
 'use strict'
-let Channel = require('../lib/channel')
-let should = require('should')
 let Bip32 = require('yours-bitcoin/lib/bip-32')
+let Bn = require('yours-bitcoin/lib/bn')
+let Channel = require('../lib/channel')
 let asink = require('asink')
+let should = require('should')
 
 describe('Channel', function () {
+  let fundingAmount = Bn(1e8)
   let myXPrv = Bip32.fromRandom()
   // let myXPub = myXPrv.toPublic()
   let theirXPrv = Bip32.fromRandom()
@@ -25,7 +27,7 @@ describe('Channel', function () {
         bob.xPrv = Bip32.fromRandom()
         carol.xPrv = Bip32.fromRandom()
 
-        bob.channel = new Channel(bob.xPrv, carol.xPrv.toPublic())
+        bob.channel = new Channel(fundingAmount, bob.xPrv, carol.xPrv.toPublic())
         yield bob.channel.asyncInitialize()
 
         // TODO: Finished
@@ -35,7 +37,8 @@ describe('Channel', function () {
 
   describe('#constructor', function () {
     return asink(function * () {
-      let channel = new Channel(myXPrv, theirXPub)
+      let channel = new Channel(fundingAmount, myXPrv, theirXPub)
+      should.exist(channel.fundingAmount)
       should.exist(channel.myXPrv)
       should.exist(channel.theirXPub)
     }, this)
@@ -44,7 +47,7 @@ describe('Channel', function () {
   describe('#asyncInitialize', function () {
     it('should set chanPath, myChanXPrv, theirChanXPub', function () {
       return asink(function * () {
-        let channel = new Channel(myXPrv, theirXPub)
+        let channel = new Channel(fundingAmount, myXPrv, theirXPub)
         yield channel.asyncInitialize()
         should.exist(channel.chanPath)
         should.exist(channel.myChanXPrv)
@@ -88,7 +91,7 @@ describe('Channel', function () {
   describe('#asyncBuildMultiSigAddr', function () {
     it('should build a multisig address', function () {
       return asink(function * () {
-        let channel = new Channel(myXPrv, theirXPub)
+        let channel = new Channel(fundingAmount, myXPrv, theirXPub)
         yield channel.asyncInitialize()
         yield channel.asyncBuildMultiSigAddr()
         should.exist(channel.multiSigAddr)
@@ -102,7 +105,7 @@ describe('Channel', function () {
       return asink(function * () {
         let myXPrv = Bip32.fromString('xprv9s21ZrQH143K3vDcUe4KsRnPzFpxwv9VhnExscaAB6KGW9kTr1YhJngKqA47ycSMzzJoGUFeh5PkC4r8nRr7iDqXSdrdh1M1rXqgFhHsFbV')
         let theirXPub = Bip32.fromString('xpub661MyMwAqRbcGsGCwFS4LxezMPgLmXQDqE5q4fUSpQ4rWSHxtQ3USe9N4AkH2x4tzoMtXiWvepZeq5AicnpqapAS68JWGJLrnrSdW5Crofo')
-        let channel = new Channel(myXPrv, theirXPub)
+        let channel = new Channel(fundingAmount, myXPrv, theirXPub)
         channel.chanPath = 'm/1/1'
         yield channel.asyncInitialize()
         yield channel.asyncBuildMultiSigAddr()
@@ -110,6 +113,17 @@ describe('Channel', function () {
 
         // 3 is the mainnet constant; tests always run on mainnet
         channel.multiSigAddr.toString().should.equal('3JPTiXjHVB5HBnUiZMXVmV4G4SkTtVgqE7')
+      }, this)
+    })
+  })
+
+  describe('#asyncGetId', function () {
+    it('should return the multisig address', function () {
+      return asink(function * () {
+        let channel = new Channel(fundingAmount, myXPrv, theirXPub)
+        yield channel.asyncInitialize()
+        let id = yield channel.asyncGetId()
+        id.should.equal(channel.multiSigAddr.toString())
       }, this)
     })
   })
