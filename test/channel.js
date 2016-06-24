adb88764382d897fb99966086d780e8eb1b0f211
Ryan X. Charles <ryan@yours.network>
2016-06-23 18:17:07 -0700

    test path derivation; fix bug

--- test/channel.js
+++ test/channel.js
@@ -23,4 +23,48 @@ describe('Channel', function () {
       should.exist(channel.theirXPub)
     }, this)
   })
+
+  describe('#asyncInitialize', function () {
+    it('should set rootPath, myXPrvRoot, theirXPubRoot', function () {
+      return asink(function * () {
+        let channel = new Channel(myXPrv, theirXPub)
+        yield channel.asyncInitialize()
+        should.exist(channel.rootPath)
+        should.exist(channel.myXPrvRoot)
+        should.exist(channel.theirXPubRoot)
+      }, this)
+    })
+  })
+
+  describe('@randomRootPath', function () {
+    it('should give a path with two numbers', function () {
+      let path = Channel.randomRootPath()
+      let [, x, y] = path.split('/').map((a) => Number(a))
+
+      // these (random) numbers should almost always be between 0 and the
+      // largest 31 bit number. technically, they could be exactly equal to 0
+      // or the largest 31 bit number, but that should almost never happen.
+      x.should.greaterThan(0)
+      x.should.lessThan(0x7fffffff)
+      y.should.greaterThan(0)
+      y.should.lessThan(0x7fffffff)
+      x.should.not.equal(y)
+    })
+  })
+
+  describe('#randomRootPath', function () {
+    it('should give a path with two numbers', function () {
+      let path = new Channel().randomRootPath().rootPath
+      let [, x, y] = path.split('/').map((a) => Number(a))
+
+      // these (random) numbers should almost always be between 0 and the
+      // largest 31 bit number. technically, they could be exactly equal to 0
+      // or the largest 31 bit number, but that should almost never happen.
+      x.should.greaterThan(0)
+      x.should.lessThan(0x7fffffff)
+      y.should.greaterThan(0)
+      y.should.lessThan(0x7fffffff)
+      x.should.not.equal(y)
+    })
+  })
 })
